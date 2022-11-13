2ca760dd89266a74dcf10efaf3679963d1183589
truong20t2 <hongtruongtyt@gmail.com>
2022-11-13 17:18:14 +0700

    firt commit

---
+++ src/test/java/food_deliverycom/cybersoft/food_delivery/FoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package food_deliverycom.cybersoft.food_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
